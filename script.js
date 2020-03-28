afa0068dc20b2b12e7bf6b89cf6d75eeb1028f16
ugtakhbaatar <upurevsuren99@gmail.com>
2020-03-28 14:33:58 -0500

    script.js updated

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+    var lenght_password = prompt("Please enter length of the password", "8");
+    if(lenght_password > 128 || lenght_password < 8) {
+        alert("Password length should between 8 and 128 characters");
+    }
+
+  var password = generatePassword(lenght_password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,4 +18,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-  
+
+function generatePassword(length) {
+    var length = length,
+        lowerCase = "abcdefghijklmnopqrstuvwxyz",
+        upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        numeric = "0123456789",
+        specialCharacters = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+
+        charset = lowerCase + upperCase + numeric + specialCharacters,
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
